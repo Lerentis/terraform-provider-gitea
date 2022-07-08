0208cbd96008477fc40c6d7dd44542104e8683df
Tobias Trabelsi <lerentis@uploadfilter24.eu>
2022-07-08 21:50:09 +0200

    #5 fixed state handling for public keys

--- gitea/resource_gitea_public_key.go
+++ gitea/resource_gitea_public_key.go
@@ -91,9 +91,9 @@ func resourcePublicKeyDelete(d *schema.ResourceData, meta interface{}) (err erro
 func setPublicKeyResourceData(pubKey *gitea.PublicKey, d *schema.ResourceData) (err error) {
 	d.SetId(fmt.Sprintf("%d", pubKey.ID))
 	d.Set(PublicKeyUser, d.Get(PublicKeyUser).(string))
-	d.Set(PublicKey, pubKey.Key)
+	d.Set(PublicKey, d.Get(PublicKey).(string))
 	d.Set(PublicKeyTitle, pubKey.Title)
-	d.Set(PublicKeyReadOnlyFlag, pubKey.ReadOnly)
+	d.Set(PublicKeyReadOnlyFlag, d.Get(PublicKeyReadOnlyFlag).(bool))
 	d.Set(PublicKeyCreated, pubKey.Created)
 	d.Set(PublicKeyFingerprint, pubKey.Fingerprint)
 	d.Set(PublicKeyType, pubKey.KeyType)
